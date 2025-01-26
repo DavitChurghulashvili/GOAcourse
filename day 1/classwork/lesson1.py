cfdf681d23fafcb7e482a34584f7909b1a79bfe7
DavitChurghulashvili <datuna.144@gmail.com>
2025-01-26 18:15:40 +0400

    first commit

---
+++ day 1/classwork/lesson1.py
@@ -0,0 +1,12 @@
+name = "davit"
+surname ="churghulashvili"
+age = 18
+height = 178.5
+knows_programming = False
+print(name + " " + surname)
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(surname))
+print(name + " " + str(age))
+print(name + " " + surname + " " + str(age) + " " + str(height))
